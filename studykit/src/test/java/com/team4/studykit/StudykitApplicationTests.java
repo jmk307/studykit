0dd81ed83f3fb4d8fd6d806f2f41ebd202105c66
jmk307 <jimin112688@gmail.com>
2023-01-20 21:26:35 +0900

    feat : 프로젝트 생성

---
+++ studykit/src/test/java/com/team4/studykit/StudykitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team4.studykit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudykitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
