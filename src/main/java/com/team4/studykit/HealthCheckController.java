d5d367ff7815cb3c41ea32150433137fdd83c9bb
jmk307 <jimin112688@gmail.com>
2023-01-21 18:20:50 +0900

    feat : EB 세팅

---
+++ src/main/java/com/team4/studykit/HealthCheckController.java
@@ -0,0 +1,16 @@
+package com.team4.studykit;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+//@Api(tags = "EB 헬스체크 컨트롤러(무시해주세용)")
+public class HealthCheckController {
+    @GetMapping
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("테스트1234");
+    }
+}
