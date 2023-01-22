c6d4233bb460d83f1291a1a9af6f735f20c7a756
jmk307 <jimin112688@gmail.com>
2023-01-22 18:56:46 +0900

    feat : util config

--- src/main/java/com/team4/studykit/HealthCheckController.java
+++ src/main/java/com/team4/studykit/HealthCheckController.java
@@ -1,14 +1,15 @@
 package com.team4.studykit;
 
-import org.springframework.http.ResponseEntity;
+import com.team4.studykit.global.config.CommonApiResponse;
+import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@Api(tags = "EB 헬스체크 컨트롤러(무시해주세용)")
+@Api(tags = "EB 헬스체크 컨트롤러(무시해주세용)")
 public class HealthCheckController {
     @GetMapping
-    public ResponseEntity<String> test() {
-        return ResponseEntity.ok("테스트124");
+    public CommonApiResponse<Boolean> healthCheck() {
+        return CommonApiResponse.of(true);
     }
 }
