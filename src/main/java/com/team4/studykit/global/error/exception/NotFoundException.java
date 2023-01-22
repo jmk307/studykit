70bd663363e42e0057f5d7930986d493d82f8020
jmk307 <jimin112688@gmail.com>
2023-01-22 18:56:33 +0900

    feat : error config

---
+++ src/main/java/com/team4/studykit/global/error/exception/NotFoundException.java
@@ -0,0 +1,25 @@
+package com.team4.studykit.global.error.exception;
+
+import com.team4.studykit.global.error.ErrorCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class NotFoundException extends BusinessException {
+    private String message;
+
+    public NotFoundException(String message) {
+        super(ErrorCode._BAD_REQUEST);
+        this.message = message;
+    }
+
+    public NotFoundException(ErrorCode errorCode, String message) {
+        super(errorCode);
+        this.message = message;
+    }
+
+    public NotFoundException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+}
